eed2935be4b9d53797d62c4500684e35444a4ae2
kilik42 <marvinevins@yahoo.com>
2022-06-05 22:42:10 -0500

    adding more to category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,3 +1,14 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
 import {GiNoodles, GiChopsticks} from 'react-icons/gi';
+import styled from "styled-components";
+import {NavLink} from 'react-router-dom';
 
+import React from 'react'
+
+function Category() {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
