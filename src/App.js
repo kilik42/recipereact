7ee4a2f1986355bf3e68729d03e28382c432a71f
kilik42 <marvinevins@yahoo.com>
2022-06-06 20:35:20 -0500

    fixed error and added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from './pages/Home'
 import Pages from './pages/Pages';
+import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-        {/* <Home /> */}
-        <Pages/>
-     
+      <BrowserRouter>
+            {/* <Home /> */}
+            <Category />
+            <Pages/>
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,20 +1,38 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
-import {GiNoodles, GiChopsticks} from 'react-icons/gi';
+import {GiNoodles,GiChopsticks} from 'react-icons/gi';
 import styled from "styled-components";
 import {NavLink} from 'react-router-dom';
-
+// import { NavLink } from 'react-router-dom';
 import React from 'react'
 
 function Category() {
   return (
-    <div>
+    <List>
 
-        <div>
+        <NavLink to ={'/cuisine/Italian'}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </div>
-    </div>
+        </NavLink>
+        <NavLink to ={'/cuisine/American'}>
+            <FaHamburger/>
+            <h4>American</h4>
+        </NavLink>
+        <NavLink to ={'/cuisine/Thai'}>
+            <GiNoodles/>
+            <h4>Thai</h4>
+        </NavLink>
+        <NavLink to ={'/cuisine/Japanese'}>
+            <GiChopsticks/>
+            <h4>Japanese</h4>
+        </NavLink>
+    </List>
   )
 }
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`
 
 export default Category
