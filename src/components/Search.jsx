d8452387fd896f2991d3113757b8ddd15b6b155b
kilik42 <marvinevins@yahoo.com>
2022-06-09 21:28:29 -0500

    begin working on search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import {useState} from 'react';
+import {FaSearch} from 'react-icons/fa';
+
+
+
+function Search() {
+  return (
+    <form>
+        <input type="text"/>
+
+    </form>
+  )
+}
+
+const FormStyle = styled.form`
+  margin: 0rem 20rem;
+  position: relative;
+  width: 100%;
+  
+  `
+
+export default Search
